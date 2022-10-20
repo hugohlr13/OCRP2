efd240f71d054030f46cec3ebbd892327b380ed3
hugohlr13 <hugo.huetleroy@gmail.com>
2022-10-20 03:01:00 +0200

    Extract books data

--- booksonline.py
+++ booksonline.py
@@ -1,4 +1,6 @@
 from calendar import c
+from genericpath import exists
+from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -12,72 +14,90 @@ def parse_html(booksonline_url):
     page_response = requests.get(booksonline_url)
     return BeautifulSoup(page_response.content, 'html.parser')
 
-def next_page_category(category_url):
-    """ Détecter la présence d'un tag next par catégorie"""
+def extract_category_urls(booksonline_url):
+    """ Obtenir les URL de chaque catégorie"""
 
-    content = parse_html(category_url)
-    next_page_tag = content.find('li', class_="next")  
-    return next_page_tag
+    content = parse_html(booksonline_url)
+    category_tags = content.findAll("ul")[2].findAll("a")
+    
+    category_href = []
+    for category_unprocessed in category_tags:
+        category_href.append(category_unprocessed["href"])
+    
+    category_urls = []
+    for first_page in category_href:
+        first_page_category = first_page.replace("../", "")
+        category_urls.append("http://books.toscrape.com/catalogue/category/" + first_page_category)
+    
+    return category_urls
 
-def extract_categories_urls(category_url):
+def extract_pages_category_urls(category_urls):
     """ Extraire les URLS des pages de chaque catégorie en détectant la présence du bouton next """
     
-    pages_categories_url = [category_url]
-    response_category = requests.get(category_url)
-    soup_category = BeautifulSoup(response_category.content, 'html.parser')
-    page_url_processed = category_url.replace("index.html", "")
+    pages_category_urls = [category_urls]
+    content = parse_html(category_urls)
+    page_url_processed = category_urls.replace("index.html", "")
     page_number = 2
 
     while True:
-        nextpage_tag = soup_category.find('li', class_="next")
+        nextpage_tag = content.find('li', class_="next")
         if nextpage_tag is None:
             break
         page_url = f'{page_url_processed}page-{page_number}.html'
-        pages_categories_url.append(page_url)
-        response_category = requests.get(page_url)
-        soup_category = BeautifulSoup(response_category.content, 'html.parser')
+        pages_category_urls.append(page_url)
+        content = parse_html(page_url)
         page_number += 1
 
-    return pages_categories_url
+    return pages_category_urls
 
-def extract_books_urls(categories_urls):
+def extract_books_urls(pages_category_urls):
     """ Extraire les URLS de tous les livres par catégorie"""
 
-    books_url = []
-    for page_url in categories_urls:
+    books_urls = []
+    for page_url in pages_category_urls:
         content = parse_html(page_url)
         titles = content.find_all("h3")
         for title in titles:
             tag_href = title.find('a')["href"]
             url = tag_href.replace("../../../", "http://books.toscrape.com/catalogue/")
-            books_url.append(url)
+            books_urls.append(url)
 
-    return books_url
+    return books_urls
 
-def extract_book_data(url):
+def extract_book_data(book_url):
     """Extraire les data d'un livre"""
 
-    book_url = url
-    url = parse_html(url)
+    url = book_url
+    book_url = parse_html(book_url)
     book_data = {}
 
-    book_data['product_page_url'] = book_url
-    book_data['universal_product_codes'] = url.find("table", class_="table table-striped").findAll("td")[0].text
-    book_data['title'] = url.find("h1").text
-    book_data['price_including_tax'] = url.find("table", class_="table table-striped").findAll("td")[3].text.lstrip("Â£")
-    book_data['price_excluding_tax'] = url.find("table", class_="table table-striped").findAll("td")[2].text.lstrip("Â£")
-    number_available_tag = url.find("table", class_="table table-striped").findAll("td")[5].text
+    book_data['product_page_url'] = url
+    book_data['universal_product_codes'] = book_url.find("table", class_="table table-striped").findAll("td")[0].text
+    book_data['title'] = book_url.find("h1").text
+    book_data['price_including_tax'] = book_url.find("table", class_="table table-striped").findAll("td")[3].text.lstrip("Â£")
+    book_data['price_excluding_tax'] = book_url.find("table", class_="table table-striped").findAll("td")[2].text.lstrip("Â£")
+    number_available_tag = book_url.find("table", class_="table table-striped").findAll("td")[5].text
     number_available_tag = re.sub(r'\D', '', str(number_available_tag))
     number_available_tag = int(number_available_tag)
     book_data['number_available'] = number_available_tag
-    book_data['product_description'] = url.findAll('p')[3].text
-    book_data['category'] = url.find('ul', class_="breadcrumb").findAll('a')[2].text
-    book_data['review_rating'] = url.findAll("p", class_="star-rating")[0]['class'][1] 
-    book_data['image_url'] = url.findAll('img')[0]['src'].replace("../..", "http://books.toscrape.com") 
+    book_data['product_description'] = book_url.findAll('p')[3].text
+    book_data['category'] = book_url.find('ul', class_="breadcrumb").findAll('a')[2].text
+    book_data['review_rating'] = book_url.findAll("p", class_="star-rating")[0]['class'][1] 
+    book_data['image_url'] = book_url.findAll('img')[0]['src'].replace("../..", "http://books.toscrape.com") 
 
     return book_data
 
-if __name__ == '__main__':
-   book_data = extract_book_data("https://books.toscrape.com/catalogue/someone-like-you-the-harrisons-2_735/index.html")
-   print(book_data)
+def extract_books_data(books_urls):
+    """ Extraire les data des livres d'une catégorie"""
+    return [extract_book_data(book_url) for book_url in books_urls]
 
+if __name__ == '__main__':
+   pages_category_urls = extract_pages_category_urls("https://books.toscrape.com/catalogue/category/books/art_25/index.html")
+   books_urls = extract_books_urls(pages_category_urls)
+   books_data = extract_books_data(books_urls)
+   print(books_urls)
+   print(len(books_urls))
+   print(books_data)
+
+
+   
